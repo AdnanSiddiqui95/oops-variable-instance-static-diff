fe2dfde0587d61c45356d985f77f17bdcf3dca29
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-07-08 23:33:09 +0530

    Add files via upload

---
+++ AA.java
@@ -0,0 +1,6 @@
+
+public class AA 
+{
+	int x=90;//Instance variable
+	static int y=60; //Class variable
+}
---
+++ BB.java
@@ -0,0 +1,12 @@
+
+public class BB {
+
+	public static void main(String[] args) 
+	{
+		AA obj=new AA();
+		System.out.println(obj.x);
+		System.out.println(AA.y);
+		System.out.println(obj.x);
+	}
+
+}
